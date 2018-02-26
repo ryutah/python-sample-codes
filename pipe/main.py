1f167edcb34ae056e36164bf52f0d8993dcec0ba
ryuta <ryuta.hara@topgate.co.jp>
2018-02-26 11:41:17 +0900

    Update: Add comment reference site

--- pipe/main.py
+++ pipe/main.py
@@ -1,5 +1,7 @@
 import sys
 
+# ref https://www.lifewithpython.com/2017/06/python-check-stdin-type.html
+
 print("Receive args : {}".format(sys.argv))
 if not sys.stdin.isatty():
     print("Receive stdin pipe :\n {}".format(sys.stdin.readlines()))
